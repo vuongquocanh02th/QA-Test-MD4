371971dd861cbc349a18a2c26ff20e6cc3afa989
Vuong Quoc Anh <vuongquocanh300402@gmail.com>
2025-02-17 09:29:51 +0700

    start project

---
+++ src/main/java/com/testmd4vqa/TestMd4VqaApplication.java
@@ -0,0 +1,13 @@
+package com.testmd4vqa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMd4VqaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestMd4VqaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/testmd4vqa/TestMd4VqaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testmd4vqa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMd4VqaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
